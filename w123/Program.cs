ed0408d4c885af82579a856707072d4da0787881
dad <sunheroaa@gmail.com>
2024-08-23 10:39:23 +0800

    jomi

--- w123/Program.cs
+++ w123/Program.cs
@@ -6,7 +6,7 @@ namespace w123
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("333388");
+            Console.WriteLine("jyamamama");
         }
     }
 }
